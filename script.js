485880585f29f86c77db2e4f030ca0ff40ba5f6c
Elena Lem <151932734+Leksa2304@users.noreply.github.com>
2024-05-12 14:57:56 +0300

    hometask-11
    доработаны ошибки по окрашиванию слова за словом, реализован подсчет ошибок и окончание игры.

--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-const words = [ // массив случайных слов
-    "солнце",
-    "успех",
-    "любовь",
-    "семья",
-    "океан",
-    "жизнь",
-    "ум",
-    "турне",
-    "лайнер",
-    "путь",
-    "щенок",
-    "бриз",
-    "веселье",
-    "дружба",
-    "радуга",
-    "деньги",
-    "удача",
-    "цветок",
-    "улыбка",
-    "радость"
-];
-
-function getword() { // функция генерации случайного слова
-    index = Math.floor(Math.random() * words.length);
-    return words[index];
-}
-
-const content = document.querySelector(".content");
+//const content = document.querySelector(".content");
 const wordContent = document.querySelector(".word");
 const сorrectWords = document.querySelector(".correct-count"); // правильно введенные слова
 const wrongWords = document.querySelector(".wrong-count"); // слова, введенные с ошибкой
 const mistakes = document.querySelector(".word-mistakes"); // количество ошибок в слове
 const timer = document.querySelector("#timer");
 
+
 let word = getword(); // случайное слово
 renderWord(word);
 
+function getword() { // функция генерации случайного слова
+    const words = [ // массив случайных слов
+        "солнце",
+        "успех",
+        "любовь",
+        "семья",
+        "океан",
+        "жизнь",
+        "ум",
+        "турне",
+        "лайнер",
+        "путь",
+        "щенок",
+        "бриз",
+        "веселье",
+        "дружба",
+        "радуга",
+        "деньги",
+        "удача",
+        "цветок",
+        "улыбка",
+        "радость"
+    ];
+
+    index = Math.floor(Math.random() * words.length);
+    return words[index];
+}
+
 function renderWord(el) { // рендеринг слова по <span>
-    const wordContent = document.querySelector(".word");
+
     wordContent.innerHTML = el.split("").map((char) => `<span>${char}</span>`).join("");
 }
 
-const spans = wordContent.querySelectorAll("span"); // spanы для каждой буквы
+
 
 let i = 0;
 let timerId;
-let seconds = 0;
-let minutes = 0;
-mistakes.textContent = 0;
-сorrectWords.textContent = 0;
-wrongWords.textContent = 0;
-
+// let seconds = 0;
+// let minutes = 0;
 
 function startTimer() {
     let [minutes, seconds] = timer.textContent.split(":").map(Number);
     seconds++;
 
-    if (seconds === 60) {
+    if (seconds === 59) {
         minutes++;
         seconds = 0;
     }
     timer.textContent = `${format(minutes)}:${format(seconds)}`
-
 }
 
 //добавление незначащих нулей
@@ -77,14 +73,14 @@ window.addEventListener("load", () => {
     timerId = setInterval(startTimer, 1000);
 });
 
-
 function paintLetter(event) { //  функция окрашивания букв
+
+    const spans = wordContent.querySelectorAll("span"); // spanы для каждой буквы
 
     if (event.key === "Shift" || event.key === "Control" || event.key === "CapsLock") return; // не принимать за ошибку нажатие смены языка и капслок
 
     if (event.key.toLowerCase() === spans[i].textContent) { // если нажата верная клавиша
-        spans[i].classList.remove("w");
-        spans[i].classList.add("c");
+        spans[i].className = "c";
         i++;
 
 
@@ -95,20 +91,52 @@ function paintLetter(event) { //  функция окрашивания букв
 
             } else { // если нет ошибок
                 сorrectWords.textContent++; // добавить к правильным словам
-
             }
 
-            spans.innerHTML = ""
-            renderWord(getword());
+            setTimeout(nextWord, 0); // функция nextWord, обернутая в setTimeout, дает возможность отработать окрашиванию последней буквы
 
         }
 
-
     } else { // если нажата неверная клавиша
-        spans[i].classList.add("w");
+
+        spans[i].className = "w";
         mistakes.textContent++; // добавлять к ошибкам
 
     }
 }
 
-document.addEventListener("keydown", paintLetter);
+function nextWord() { // функция следующего слова
+    //проверка - не закончилась ли игра(написать функцию checkWordsCount)
+    checkWordsCount();
+
+    // рендерим новое слово
+
+    renderWord(getword());
+
+    // обнуляем индекс
+    i = 0;
+    // обнуляем ошибки в слове
+    mistakes.textContent = 0;
+
+}
+
+function checkWordsCount() { // проверка на окончание игры
+    // Если количество неправильно введенных 5, то ....
+    if (wrongWords.textContent == 5) {
+
+        clearTimeout(timerId);
+        alert("Вы проиграли");
+        location.reload(); // перезагрузка страницы для начала новой игры
+    }
+
+    // Если количество правильно введенных слов 5, то ....
+    if (сorrectWords.textContent == 5) {
+
+        clearTimeout(timerId);
+        alert(`Победа. Ваше время: ${timer.textContent}`);
+        location.reload(); // перезагрузка страницы для начала новой игры
+
+    }
+}
+
+document.addEventListener("keypress", paintLetter); // обработчик на нажатие клавиши
